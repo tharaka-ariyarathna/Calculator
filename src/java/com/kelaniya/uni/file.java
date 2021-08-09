faf87fbe83858339ba4bb3c884d05bec8206d6d4
Tharaka-ariyarathna <tharakaariyarathna04@gmail.com>
2021-08-09 10:19:21 +0530

    Adding 3 new functions

---
+++ src/java/com/kelaniya/uni/file.java
@@ -0,0 +1,22 @@
+package com.kelaniya.uni;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class file {
+    Scanner scanner ;
+
+    public void openFile(){
+        try{
+            scanner = new Scanner(new File("Numbers.txt")) ;
+        }catch (Exception e){
+            System.out.println("Could't read from file");
+        }
+    }
+
+    public int readFile(){
+        String num = scanner.next() ;
+        int number = Integer.parseInt(num) ;
+        return number ;
+    }
+}
---
+++ src/java/com/kelaniya/uni/userInput.java
@@ -0,0 +1,2 @@
+package com.kelaniya.uni;public class userInput {
+}
