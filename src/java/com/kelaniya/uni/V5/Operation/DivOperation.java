b4db352b81a3e26e80421dc266947bd22f8129f7
Tharaka-ariyarathna <tharakaariyarathna04@gmail.com>
2021-09-20 19:08:57 +0530

    Started unit testing

---
+++ src/java/com/kelaniya/uni/V5/Operation/DivOperation.java
@@ -0,0 +1,2 @@
+package com.kelaniya.uni.V5.Operation;public class DivOperation {
+}
---
+++ src/java/com/kelaniya/uni/V5/Operation/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package com.kelaniya.uni.V5.Operation;
+
+public class InvalidOperationException extends Throwable {
+    public InvalidOperationException(String message) {
+
+    }
+}
